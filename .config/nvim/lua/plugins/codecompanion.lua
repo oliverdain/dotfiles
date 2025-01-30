c4080d80b440933ed0693259f83ffde365175360
Oliver Dain <oliver@dains.org>
2025-01-30 09:59:49 -0800

    Added shortcuts for CodeCompanion stuff.

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -1,4 +1,8 @@
 return {
    "olimorris/codecompanion.nvim",
-   dependencies = { "nvim-treesitter/nvim-treesitter" }
+   dependencies = { "nvim-treesitter/nvim-treesitter" },
+   init = function()
+      vim.api.nvim_create_user_command("Ccc", "CodeCompanionChat <args>", {nargs="*"})
+      vim.api.nvim_create_user_command("Cc", "CodeCompanion <args>", {nargs="*"})
+   end
 }
