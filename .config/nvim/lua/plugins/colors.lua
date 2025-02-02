a26cd1326b868f30df058e1c56772eedfb4d2149
Oliver Dain <oliver@dains.org>
2025-02-01 16:39:59 -0800

    Fixed color schemes.

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -5,6 +5,11 @@ return {
       "NLKNguyen/papercolor-theme",
       priority = 1000,
       lazy = false,
+      -- The setup here is a little odd. For some reason if I put the colorscheme line as a config function it'd set the
+      -- colorscheme (e.g. :colorscheme would show the right string) but it wouldn't take effect. But putting it in init
+      -- made it take effect only like 1/2 the time - totally at random *something* would change the colorscheme but I
+      -- couldn't figure out what. So we *also* put a UIEnter autocmd in the config block to re-load the colorscheme if
+      -- something did overwrite it.
       init = function()
          -- Color scheme and underline spelling errors instead of highlighting them
          -- Other colorscheme's I've liked: apprentice and jellybean
@@ -14,6 +19,13 @@ return {
          vim.cmd([[set background=dark]])
          vim.cmd([[colorscheme PaperColor]])
          vim.cmd([[autocmd ColorScheme * hi clear SpellBad | hi SpellBad cterm=underline gui=underline]])
-      end
+      end,
+      config = function()
+         vim.api.nvim_create_autocmd("UIEnter", {
+           callback = function()
+             vim.cmd("colorscheme PaperColor")
+           end
+         })
+      end,
    },
 }
